072141872e8eb37bc4d348564e41af1f8742517c
brcopeland <bc2675@cumc.columbia.edu>
2016-04-11 19:29:33 -0400

    Updated to use Ensembl v.74.

--- annodb/src/translate_transcripts.pl
+++ annodb/src/translate_transcripts.pl
@@ -3,32 +3,32 @@
 use strict;
 
 use lib '/nfs/goldstein/goldsteinlab/software/bioperl-1.2.3';
-use lib '/nfs/goldstein/goldsteinlab/software/ensembl_73/modules';
-use lib '/nfs/goldstein/goldsteinlab/software/ensembl-variation_73/modules';
+use lib '/nfs/goldstein/software/ensembl_74/modules';
+use lib '/nfs/goldstein/software/ensembl-variation_74/modules';
 use lib '/nfs/goldstein/software/perlbrew/perls/perl-5.20.2/lib/site_perl/5.20.2';
 
 use Bio::EnsEMBL::Registry;
 use Digest::MD5 qw(md5_hex);
 use Config::Simple;
 
-my $cfg = new Config::Simple("/nfs/goldstein/software/annodb/.my.cnf");
+my $cfg = new Config::Simple("/nfs/goldstein/software/dragen/.my.cnf");
 
 my $reg = 'Bio::EnsEMBL::Registry';
 $reg->load_registry_from_db(
-	-host => $cfg->param("clienthomo_sapiens_variation_73_37.host"),
-	-user => $cfg->param("clienthomo_sapiens_variation_73_37.user"),
-	-pass => $cfg->param("clienthomo_sapiens_variation_73_37.password"),
-	-db_version => '73'
+	-host => $cfg->param("clienthomo_sapiens_variation_74_37.host"),
+	-user => $cfg->param("clienthomo_sapiens_variation_74_37.user"),
+	-pass => $cfg->param("clienthomo_sapiens_variation_74_37.password"),
+	-db_version => '74'
 );
 
 my $transcript_adaptor = $reg->get_adaptor('Human', 'Core', 'Transcript');
-my $dbh = DBI->connect("DBI:mysql:homo_sapiens_core_73_37;mysql_read_default_file=/nfs/goldstein/software/annodb/.my.cnf;mysql_read_default_group=clienthomo_sapiens_variation_73_37", undef, undef);
+my $dbh = DBI->connect("DBI:mysql:homo_sapiens_core_74_37;mysql_read_default_file=/nfs/goldstein/software/annodb/.my.cnf;mysql_read_default_group=clienthomo_sapiens_variation_74_37", undef, undef);
 my $query = "SELECT stable_id FROM transcript";
 my $sth = $dbh->prepare($query);
 $sth->execute();
 my $results = $sth->fetchall_arrayref;
 $sth->finish();
-my $dbh2 = DBI->connect("DBI:mysql:homo_sapiens_variation_73_37;mysql_read_default_file=/nfs/goldstein/software/annodb/.my.cnf;mysql_read_default_group=clienthomo_sapiens_variation_73_37", undef, undef);
+my $dbh2 = DBI->connect("DBI:mysql:homo_sapiens_variation_74_37;mysql_read_default_file=/nfs/goldstein/software/annodb/.my.cnf;mysql_read_default_group=clienthomo_sapiens_variation_74_37", undef, undef);
 my $md5_query = "SELECT translation_md5_id FROM translation_md5 WHERE translation_md5 = ?";
 my $md5_sth = $dbh2->prepare($md5_query);
 my $stable_id = "";
