dd6fdb11611c3752e30a385a85a21c46c6c32512
brcopeland <bc2675@cumc.columbia.edu>
2016-04-12 16:26:02 -0400

    Added dict mapping of column name to column number in a vcf.

--- annodb/src/dragen_globals.py
+++ annodb/src/dragen_globals.py
@@ -5,6 +5,7 @@ Constant variables shared across modules for the DRAGEN pipeline
 CNF = "/nfs/goldstein/software/dragen/dragen.cnf" # defaults file for pipeline
 VCF_COLUMNS = ["CHROM", "POS", "rs_number", "REF", "ALT", "QUAL", "FILTER",
                "INFO", "FORMAT", "call"]
+VCF_COLUMNS_DICT = dict([(column, x) for x, column in enumerate(VCF_COLUMNS)])
 BLOCK_SIZE = 10000 # the bases in a block of variant calls (for indexing)
 VALID_GTS = set(["0", "1"]) # valid values in GT field, i.e. REF/ALT
 # the table format to output for calls
