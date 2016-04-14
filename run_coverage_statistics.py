da93b381af553b5e1c241650392199372e69c9c3
rpadmanabhan <raghus606@gmail.com>
2016-04-14 16:08:37 -0400

    Made multiple small changes to the functions, this version works!

--- run_coverage_statistics.py
+++ run_coverage_statistics.py
@@ -11,11 +11,17 @@ def get_filename_from_fullpath(full_path):
     
     return full_path.split('/')[-1]
 
+def run_cmd(cmd_to_run):
+    ''' Run a command
+    '''
+    os.system(cmd_to_run) ## Replace this with subprocess 
+
 def create_targetfile(options):
     ''' Use this function to create a target interval file list
     compatible with GATK and Picard Tools
     '''
     ## Unpack arguments for BedToIntervalList
+    java_location = options.java_location
     picard_location = options.picard_location
     bed_file = options.bed_file 
     seqdict_file = options.seqdict_file
@@ -24,40 +30,52 @@ def create_targetfile(options):
     output_name = get_filename_from_fullpath(bed_file)
     output_file = options.output_dir + output_name + ".list"
   
-    bedtointerval_cmd = "java -jar %s BedToIntervalList I=%s SD=%s OUTPUT=%s"%(picard_location,bed_file,seqdict_file,output_file)
-    os.system(bedtointerval_cmd)
+    bedtointerval_cmd = "%s -jar %s BedToIntervalList I=%s SD=%s OUTPUT=%s"%(java_location,picard_location,bed_file,seqdict_file,output_file)
+    run_cmd(bedtointerval_cmd)
+
 
 def run_wgsmetrics(options):
-    ''' Run GATK's DepthOfCoverage Function for
+    ''' Run Picard's CollectWgsMetrics Function for
     Genome sequencing samples
     '''
 
-    ## Unpack arguments for DepthOfCoverage
+    ## Unpack arguments
+    java_location = options.java_location
     picard_location = options.picard_location
     reference_file = options.reference_file
     bam_file = options.bam_file 
     output_file = options.output_dir + options.sample +".metrics.txt"
-    java_location = options.java_location 
+ 
+    ## Define the CollectWgsMetrics Command
+    if(options.wgsinterval == True):
+        target_file = options.target_file
+        wgs_cmd = "%s -jar %s CollectWgsMetrics R=%s O=%s I=%s INTERVAL=%s MQ=20 Q=10"%(java_location,picard_location,reference_file,output_file,bam_file,target_file)
+    else:
+        wgs_cmd = "%s -jar %s CollectWgsMetrics R=%s O=%s I=%s MQ=20 Q=10"%(java_location,picard_location,reference_file,output_file,bam_file)
 
-    depthofcoverage_cmd = "%s -jar %s CollectWgsMetrics -R=%s -O=%s -I=%s MQ=20 Q=10"%(java_location,picard_location,reference_file,output_file,bam_file,target_file)
-    os.system(depthofcoverage_cmd)
+    ## Run the command
+    run_cmd(wgs_cmd)
+
 
 def run_hsmetrics(options):
     ''' RUN HsMetrics 
     '''
     ## Unpack arguments for hsmetrics
-    picard_location = options.picard_dir
+    java_location = options.java_location
+    picard_location = options.picard_location
     bait_file = options.bait_file
     target_file = options.target_file
     bam_file = options.bam_file
     sample = options.sample
     output_file = options.scratch + options.sample + ".hsmetrics.txt" 
 
-    ## Define the command to run
-    hsmetrics_cmd = "java -jar %s CalculateHsMetrics BI=%s TI=%s METRIC_ACCUMULATION_LEVEL=ALL_READS I=%s O=%s"%(picard_dir,baits_file,targets_file,bam_file,output_dir)
-
     
-    ## Run the command 
+    ## Define the command to run
+    hsmetrics_cmd = "%s -jar %s CalculateHsMetrics BI=%s TI=%s METRIC_ACCUMULATION_LEVEL=ALL_READS I=%s O=%s MQ=20 Q=10"%(java_location,picard_location,bait_file,target_file,bam_file,output_file)
+    
+    ## Run the command
+    run_cmd(hsmetrics_cmd) 
+    
     
 def process_metricsfile():
     ''' Process the HsMetrics Output
@@ -70,14 +88,12 @@ def output_metrics():
 def main(options):
     ''' Main Function
     '''
-
-    
+   
     if(options.create_targetfile == True):
         create_targetfile(options)
 
-
     if(options.seq_type.upper() == 'GENOME'):
-        run_depthofcoverage(options)
+        run_wgsmetrics(options)
     
     elif(options.seq_type.upper() == 'EXOME'):
         run_hsmetrics(options)
@@ -95,7 +111,8 @@ if __name__ == '__main__':
     parser.add_option("--scratch", type = "string",help="Scratch directory", dest = "scratch", default = ".")
     parser.add_option("--bamfile", type = "string",help="BAM file",dest = "bam_file",default = "")
     parser.add_option("--bed", type = "string",help="BED file", dest = "bed_file",default = "")
-    parser.add_option("--target", type = "string",help="Target List File(Specify the flag to create it if you dont have one)", dest = "target_file", default = "")
+    parser.add_option("--target_file", type = "string",help="Target List File(Specify the flag to create it if you dont have one)", dest = "target_file", default = "")
+    parser.add_option("--bait_file", type = "string",help="Bait File", dest = "bait_file", default = "")
     parser.add_option("--reference", type = "string",help="Reference Fasta", dest = "reference_file", default = "")
     parser.add_option("--seq_dictionary", type = "string",help="Sequence Dictionary",dest = "seqdict_file",default="")
     parser.add_option("--sample", type = "string",help="SAMPLE ID",dest = "sample",default="")
@@ -104,10 +121,11 @@ if __name__ == '__main__':
     parser.add_option("--pythondir", type = "string",help ="Python script directory",dest= "pythondir",default = ".")
     parser.add_option("--output",type="string",dest = "output_dir",default = ".")
     parser.add_option("--cluster",type="string",dest = "cluster",default="lsrc")
-    parser.add_option("--create_targetfile",action="store_true",help="FLAG,True/False",dest="create_targetfile",default="False")
+    parser.add_option("--create_targetfile",action="store_true",help="FLAG-True/False",dest="create_targetfile",default="False")
     parser.add_option("--picard_location",type="string",help="Picard jar full path",dest="picard_location",default="")
-    parser.add_option("--gatk_location",type="string",help="GATK jar full path",dest="gatk_location",default="")
-    
+    parser.add_option("--java_location",type="string",help="Java full path",dest="java_location",default="")
+    parser.add_option("--wgsinterval",action="store_true",help="FLAG-True/False",dest="wgsinterval",default="False")
+
     (options,args) = parser.parse_args()
 
     #if(len(options)<2):
@@ -133,6 +151,9 @@ if __name__ == '__main__':
     if (options.target_file == "" and options.create_targetfile==False):
         print "No Target File Provided - exiting..."
         sys.exit()    
+    if (options.bait_file == "" and options.seq_type.upper() == 'EXOME'):
+        print "No Bait File Provided - exiting..."
+        sys.exit()
     if (options.sample == "" and options.create_targetfile==False):
         print "No sample id - Exiting..."
         sys.exit()
@@ -143,8 +164,8 @@ if __name__ == '__main__':
     if (options.picard_location == ""):
         print "Specify Picard Location !\nexiting ..."
         sys.exit()
-    if (options.gatk_location == ""):
-        print "Specify GATK location !\nexiting ..."
+    if (options.java_location == ""):
+        print "Specify Java location !\nexiting ..."
         sys.exit()
     if (options.seqdict_file == "" and create_targetfile == True):
         print "Specify the Sequence Dictionary File To Use!\nexiting ..."
