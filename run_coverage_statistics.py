d13e8e9112cba23b0b1c47107db40abff3a3f994
rpadmanabhan <raghus606@gmail.com>
2016-04-19 15:12:34 -0400

    Updated the script to use the regionstobed function

--- run_coverage_statistics.py
+++ run_coverage_statistics.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env Python
+#!/usr/bin/env python
 import sys
 from optparse import OptionParser 
 import subprocess
 import os 
-
+import regionstobed 
 
 def get_filename_from_fullpath(full_path):
     ''' Get just the filename from a full path file
@@ -71,6 +71,7 @@ def run_wgsmetrics(options):
     ## Define the CollectWgsMetrics Command
     if(options.wgsinterval == True):
         if(options.create_targetfile == True):
+            bed_file = options.bed_file
             output_name = get_filename_from_fullpath(bed_file)
             target_file = options.output_dir + output_name + ".list"
         else:    
@@ -98,6 +99,7 @@ def run_hsmetrics(options):
     output_file = options.scratch + options.sample + ".hsmetrics.txt" 
 
     if(options.create_targetfile == True):
+        bed_file = options.bed_file 
         output_name = get_filename_from_fullpath(bed_file)
         target_file = options.output_dir + output_name + ".list"
     else:    
@@ -137,7 +139,7 @@ def process_wgsmetrics(options):
     '''
 
     i=0
-    metrics_file = options.scratch + options.sample + ".metrics.txt"
+    metrics_file = options.output_dir + options.sample + ".metrics.txt"
     metrics = []
     headers = []
     with open(metrics_file) as IN:
@@ -152,23 +154,46 @@ def process_wgsmetrics(options):
     
     wgsmetrics = dict(zip(headers,metrics))
     ## Output required columns (PCT_1X,PCT_5X,PCT_10X,PCT_15X,PCT20X,MEAN_COVERAGE)
+
+def create_bed(options):
+    ''' Uses the regionstobed function to create a bed file
+    naming scheme for the output bed is simply to append a .bed
+    to the regions file name, output is in the same directory as regions file
+    Regions file is assumed to have the absolute path, returns the full path to 
+    the bed file
+    '''
+
+    output_bed_file = options_regions_file + ".bed"
+    regionstobed.output_bed(options.regions_file,output_bed_file)
     
+    return output_bed_file  
     
 def main(options):
     ''' Main Function
     '''
    
     if(options.create_targetfile == True):
+        print "\nCreating The Target Intervals File....\n"
+        if options.bed_file == "" and len(options.regions_file > 1): ## If the regions file is used
+            options.bed_file = create_bed(options)
         create_targetfile(options)
-        print "Created TargetFile\n"
+        print "\nCreated TargetFile\n"
         
     if(options.seq_type.upper() == 'GENOME'):
+        print "\nRunning wgsmetrics as this is a GENOME sample....\n"
+        if options.wgsinterval == True:
+            if options.bed_file == "" and len(options.regions_file > 1): ## If the regions file is used
+                options.bed_file = create_bed(options) 
+                            
         run_wgsmetrics(options)
         process_wgsmetrics(options)
-    
-    else:
+        print "\nFinished Running Metrics\n"
+        
+    elif(options.seq_type.upper()!= ''):
+        print "\nRunning hsmetrics as this is a EXOME/CUSTOM_CAPTURE....\n"
         run_hsmetrics(options)
         process_hsmetrics(options)
+        print "\nFinished Running Metrics\n"
         
     
 if __name__ == '__main__':
@@ -188,10 +213,10 @@ if __name__ == '__main__':
     parser.add_option("--output",type="string",dest = "output_dir",default = ".")
     parser.add_option("--cluster",type="string",dest = "cluster",default="lsrc")
     parser.add_option("--create_targetfile",action="store_true",help="FLAG-True/False",dest="create_targetfile",default="False")
-    parser.add_option("--picard_location",type="string",help="Picard jar full path",dest="picard_location",default="")
-    parser.add_option("--java_location",type="string",help="Java full path",dest="java_location",default="")
+    parser.add_option("--picard_location",type="string",help="Picard jar full path",dest="picard_location",default="/nfs/goldstein/software/picard-tools-2.2.1/picard.jar")
+    parser.add_option("--java_location",type="string",help="Java full path",dest="java_location",default="/nfs/goldstein/software/jdk1.8.0_05/bin/java")
     parser.add_option("--wgsinterval",action="store_true",help="FLAG-True/False",dest="wgsinterval",default="False")
-
+    parser.add_option("--regions_file",action="string",help="ATAV Regions File",dest="regions_file",default="")
     (options,args) = parser.parse_args()
         
         
@@ -209,10 +234,10 @@ if __name__ == '__main__':
     if (options.bam_file == "" and options.seq_type!=""):
         print "No BAM file - exiting..."
         sys.exit()   
-    if (options.bed_file == "" and options.create_targetfile==True):
-        print "No BED file provided to create TargetList File - exiting ..."
+    if (options.bed_file == "" and options.create_targetfile==True and options.regions_file == ""):
+        print "No BED file or Regions File provided to create TargetList File - exiting ..."
         sys.exit()
-    if (options.target_file == "" and options.create_targetfile==False):
+    if (options.target_file == "" and options.create_targetfile==False and options.seq_type.upper()!="GENOME"):
         print "No Target File Provided - exiting..."
         sys.exit()    
     if (options.bait_file == "" and options.seq_type.upper() not in ['GENOME','']):
@@ -221,16 +246,17 @@ if __name__ == '__main__':
     if (options.sample == "" and options.seq_type.upper()!=''):
         print "No sample id - Exiting..."
         sys.exit()
+    if (options.wgsinterval == True and options.bed_file == "" and options.regions_file == ""):
+        print "No region specified for wgsmetrics\nTurn of flag if the whole genome metrics are required - Exiting..."
+        sys.exit()
     if (options.prepid == ""):
         print "No Prep ID"
     if (options.seq_type == ""):
         print "No SEQ type - No Metrics will be run"
-    if (options.picard_location == ""):
-        print "Specify Picard Location !\nexiting ..."
-        sys.exit()
-    if (options.java_location == ""):
-        print "Specify Java location !\nexiting ..."
-        sys.exit()
+    if (options.picard_location == "/nfs/goldstein/software/picard-tools-2.2.1/picard.jar"):
+        print "Using default Picard location : %s"%(options.picard_location)
+    if (options.java_location == "/nfs/goldstein/software/jdk1.8.0_05/bin/java"):
+        print "Using default Java location : %s"%(options.java_location)
     if (options.seqdict_file == "" and create_targetfile == True):
         print "Specify the Sequence Dictionary File To Use For Creating the TargetFile!\nexiting ..."
         sys.exit()
