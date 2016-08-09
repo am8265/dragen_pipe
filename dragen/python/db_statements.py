d6836766eaeb43dbc194ab331bcf0a7a81069cf3
joshbridgers <joshbridgers@gmail.com>
2016-08-08 21:58:03 -0400

    changed sample_id to pseudo_prepid

--- dragen/python/db_statements.py
+++ dragen/python/db_statements.py
@@ -1,44 +1,44 @@
 GET_SAMPLE_INFO = """
 SELECT sample_name, sample_type, capture_kit, prep_id
 FROM sample
-WHERE sample_id = {sample_id}
+WHERE pseudo_prepid = {pseudo_prepid}
 """
 GET_PIPELINE_STEP_ID = """
 SELECT id
 FROM pipeline_step
-WHERE description = "Imported Chromosome {chromosome}"
+WHERE step_name = "{step_name}"
 """
 GET_PIPELINE_FINISHED_ID = """
 SELECT id
 FROM pipeline_step
-WHERE description = "Sample Finished"
+WHERE step_name = "ArchiveSample""
 """
 GET_STEP_STATUS = """
 SELECT finished
 FROM sample_pipeline_step
-WHERE sample_id = {sample_id} AND pipeline_step_id = {pipeline_step_id}
+WHERE pseudo_prepid = {pseudo_prepid} AND pipeline_step_id = {pipeline_step_id}
 """
 INSERT_PIPELINE_STEP = """
-INSERT INTO sample_pipeline_step (sample_id, pipeline_step_id)
-VALUE ({sample_id}, {pipeline_step_id})
+INSERT INTO sample_pipeline_step (pseudo_prepid, pipeline_step_id)
+VALUE ({pseudo_prepid}, {pipeline_step_id})
 """
 UPDATE_PIPELINE_STEP_SUBMIT_TIME = """
 UPDATE sample_pipeline_step
 SET submit_time = NOW()
-WHERE sample_id = {sample_id} AND pipeline_step_id = {pipeline_step_id}
+WHERE pseudo_prepid = {pseudo_prepid} AND pipeline_step_id = {pipeline_step_id}
 """
 UPDATE_PIPELINE_STEP_FINISH_TIME = """
 UPDATE sample_pipeline_step
 SET finish_time = NOW(), finished = 1
-WHERE sample_id = {sample_id} AND pipeline_step_id = {pipeline_step_id}
+WHERE pseudo_prepid = {pseudo_prepid} AND pipeline_step_id = {pipeline_step_id}
 """
 UPDATE_PIPELINE_FINISH_SUBMIT_TIME = """
 UPDATE sample_pipeline_step s1
 INNER JOIN (
-SELECT sample_id, MIN(submit_time) as st
+SELECT pseudo_prepid, MIN(submit_time) as st
 FROM sample_pipeline_step
-WHERE sample_id = {sample_id} AND pipeline_step_id <> {pipeline_step_id}
-GROUP BY sample_id) AS s2
-ON s1.sample_id = s2.sample_id AND s1.pipeline_step_id = {pipeline_step_id}
+WHERE pseudo_prepid = {pseudo_prepid} AND pipeline_step_id <> {pipeline_step_id}
+GROUP BY pseudo_prepid) AS s2
+ON s1.pseudo_prepid = s2.pseudo_prepid AND s1.pipeline_step_id = {pipeline_step_id}
 SET s1.submit_time = s2.st
 """
