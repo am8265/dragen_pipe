5a74efff89177a14475591e9d4f6cf956634585d
rpadmanabhan <raghus606@gmail.com>
2016-08-31 15:41:49 -0400

    Now read config file from LUIGI_CONFIG_PATH

--- dragen/python/dragen_globals.py
+++ dragen/python/dragen_globals.py
@@ -5,10 +5,18 @@ Constant variables shared across modules for the DRAGEN pipeline
 import MySQLdb
 import os
 from ConfigParser import ConfigParser
+import subprocess
 
 cfg = ConfigParser()
-cfg.read("/home/jb3816/github/dragen_pipe/dragen/python/luigi.cfg")
 
+### Get luigi config file from the environment variable 
+proc = subprocess.Popen("echo $LUIGI_CONFIG_PATH",shell=True,stdout=subprocess.PIPE)
+proc.wait()
+if proc.returncode: ## Non zero return code
+    raise subprocess.CalledProcessError(proc.returncode,"echo $LUIGI_CONFIG_PATH\nCould not find luigi config file, check your path!\n")
+
+config_file = proc.stdout.read().strip('\n')
+cfg.read(config_file)
 
 
 def get_connection(db):
