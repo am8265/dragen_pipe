6d50a15b61975b1f688363f27c97465ae5fa3c1d
rpadmanabhan <raghus606@gmail.com>
2016-09-09 11:54:04 -0400

    Updated code, now ordering in the input map file is maintained, take care of missing variants as well, runs slow (12 mins) need to fix this

--- create_ped_map.py
+++ create_ped_map.py
@@ -4,7 +4,7 @@ import argparse
 import gzip
 import bz2
 import os
-from collections import defaultdict
+from collections import defaultdict,OrderedDict
 
 ########################################################################
 ###                                                                  ###
@@ -63,14 +63,99 @@ def is_multiallelic(gt):
         return True
 
 def is_nonref_alt(alt):
-    """ Checks whether the alt allele is the string <NON_REF>, if so return True else False
+    """ Checks whether the alt allele is the string '<NON_REF>', if so return True else False
     
     Args : alt ; string ; the alternate allele column in the vcf file 
     Returns : bool  
     """
 
     return (alt == 'NON_REF')
+
+def get_info_key(info,key,field):
+    """ The last 2 columns in a vcf/gvcf file are like : 
+    GT:DP:GQ:MIN_DP:PL    0/0:2:6:20,6,65
+    Here GT would be 0/0, DP would be 2 , GQ 6 and so on
+    There is not set ordering required hence the values in the last column
+    are ordered according to the names in the penultimate column. This function returns
+    the position of the info key(say GT) in a given info field. So for the above e.g. if
+    the key specified was GQ this function will return 2(the indexing is 0 based), this can 
+    then be used to query the final column for the requisite key value
+
+    Args : info ; string ; the penultimate column of a vcf/gvcf file 
+           key ; string ; the specific info key for which we need the index
+    Returns : int ; the index(0 based) of the key in the info column   
+    """
+
+    try:
+        return field.split(':')[info.split(':').index(key)]
+    except ValueError as e: ## Need to look for stray cases where DP is not defined in the info field, it is (atleast in the cases i have encountered) 0 in such cases 
+        return 0
+
+def get_genotypes(ref,alt,gt):
+    """
+    Uses the GT field of a vcf files and returns the appropriate genotypes for the 2 alleles
+    Note : I am assuming the ref allele to be the allele1 in the plink file 
+
+    Args: ref; string ; the reference allele genotype
+          alt; string ; the alternate allele genotype
+          gt; the GT field in the vcf file specifying whether the variant is hom_ref,hom_alt,etc.
+    """
+
+    if gt == '0/0':
+        return [ref,ref]
+    elif gt == '1/1':
+        return [alt,alt]
+    elif gt == '1/0' or gt == '0/1':
+        return [ref,alt]
+    else: ## Multi allelic site
+        print Exception("Warning ! Caught a multiallelic flag through the filter-Check code/vcf")
+        return [-1,-1]
+
+def update_missing_variants(ped_list,map_list,map_info):
+    """ Helper function for updating missing entries
+    Args: ped_list ; list ; contains output ped info
+          map_list ; list of tuples ; contains output map info
+          map_info ; tuple ; contains info for the map file
+    Returns: list,list of tuples; update and return input data structures 
+    """
+    ped_list.extend(['0','0'])
+    map_list.append(map_info)
+    return [ped_list,map_list]
     
+def return_pedmap(gvcf_hashmap,coordinates):
+    """ Create the genotypes and loci for the ped map files by processing the gvcf_hashmap at the input coordinates
+
+    Args: gvcf_hashmap; dictionary of lists ; key : 'chrom_pos', val : [rsid,ref,alt,gt,dp,variant_type]
+          coordinates; dictionary; key: 'chrom_pos', val: 0/1
+    Returns: List, List of tuples ; the ped file genotypes, the map file info ; these can later be processed for outputing a ped file
+    """
+    
+    temp_ped = []
+    temp_map = []
+        
+    for key in coordinates:
+        chrom,pos = key.split('_')                    
+        if key in gvcf_hashmap: ## Site is present in gvcf file
+            rsid,ref,alt,gt = gvcf_hashmap[key][0:4]
+            if gvcf_hashmap[key][-1] == 'nonref': ## Non ref site
+                if int(gvcf_hashmap[key][-2]) >= 10: ## Check dp
+                    temp_ped.extend([ref,ref])
+                    temp_map.append((chrom,rsid,'0',pos))
+            else: ## A variant site
+                if not is_multiallelic(gt) and is_valid_snv(ref,alt):
+                    allele1,allele2 = get_genotypes(ref,alt,gt)
+                    if allele1 == -1 and allele2 == -1: ## Stringent check,set to missing
+                        temp_ped,temp_map = update_missing_variants(temp_ped,temp_map,(chrom,rsid,'0',pos))
+                    temp_ped.extend(get_genotypes(ref,alt,gt))
+                    temp_map.append((chrom,rsid,'0',pos))
+                else: ## Consider the site to be missing
+                    temp_ped,temp_map = update_missing_variants(temp_ped,temp_map,(chrom,rsid,'0',pos))
+        else: ## Site is missing
+            rsid = coordinates[key]
+            temp_ped,temp_map = update_missing_variants(temp_ped,temp_map,(chrom,rsid,'0',pos))
+            
+    return [temp_ped,temp_map]
+                                        
 def parse_gvcf(line):
     """ Parse a gvcf line to get : 
     Chromosome , Position, Ref, Alt, GT
@@ -85,63 +170,79 @@ def parse_gvcf(line):
     Returns ; list ; [chromosome, position, reference allele, alternate allele, genotype(GT)]
     """
 
-    chrom,rsid,pos,ref,alt = line.strip('\n').split('\t')[0:5]
-    gt = line.strip('\n').split('\t')[-1].split(':')[0] ## Maybe generalize this later based on the string GT in the penultimate column
-    return [chrom,rsid,pos,ref,alt,gt]
+    contents = line.strip('\n').split('\t')
+    chrom,pos,rsid,ref,alt = contents[0:5]
+    info = contents[-2]
+    info_contents = info.split(':')
+    if len(info_contents) == 5: ## A non ref site
+        end = contents[-3].split('=')[1]
+        alt_type = 'nonref'
+    else: ## A ref site
+        end = pos
+        alt_type = 'alt'
+    val_field = contents[-1]
+    gt = get_info_key(info,'GT',val_field)
+    dp = get_info_key(info,'DP',val_field)
+    interval = int(end) - int(pos) + 1
+
+    if alt_type == 'alt':
+        yield [chrom,pos,rsid,ref,alt,gt,dp,'alt']
+    else:
+        for _ in range(interval):
+            yield [chrom,pos,rsid,ref,alt,gt,dp,'nonref']
+            pos = str(int(pos) + 1)
 
 def print_output(line,output_file):
     """ Print output line to file
 
     Args : line ; string ; the line(s) to print
     """
-
+    
     with open(output_file,'w') as OUT:
         OUT.write(line+'\n')
            
-def iterate_gvcf(gvcf,positions):
+def iterate_gvcf(gvcf):
     """ Iterate over the gvcf file and keep storing the genotype and marker loci 
     information
     
     Args : gvcf ; string ; path to the gvcf file 
-    
-    Returns : (list,tuple) ; the genotypes and the marker loci
+    Returns : dict;
     """
 
     genotypes_ped = []
     locations_map = []
+
+    gvcf_hashmap = {}
     
     with open_by_magic(gvcf) as GVCF:
         for line in GVCF:
             if line[0]!='#': ## Skip header
-                chrom,pos,rsid,ref,alt,gt = parse_gvcf(line)
-                if alt.find(',')!=-1: ## Potentially multi-allelic, use the first allele
-                    alt = alt.split(',')[0]
-                ## Check if the site is valid
-                if (not is_nonref_alt(alt) and  not is_multiallelic(gt) and is_valid_snv(ref,alt) and positions[chrom+'_'+pos] == 0):
-                    if gt == '0/0':
-                        genotypes_ped.extend((ref,ref))
-                        locations_map.append((chrom,rsid,'0',pos))
-                    elif gt == '1/0':
-                        genotypes_ped.extend((ref,alt))
-                        locations_map.append((chrom,rsid,'0',pos))
-                    elif gt == '1/1':
-                        genotypes_ped.extend((alt,alt))
-                        locations_map.append((chrom,rsid,'0',pos))
-
-    return (genotypes_ped,locations_map)
+                gvcf_generator = parse_gvcf(line) ## If the site is multiallelic there could be more than one return 
+                for element in gvcf_generator: 
+                    chrom,pos,rsid,ref,alt,gt,dp,alt_type = element
+                    ## Better to store only valid sites so as to reduce space complexity
+                    if alt.find(',')!=-1: ## A multi-allelic site, use the first allele, this could be the alt,<NON_REF> case , but these are still valid since the GT field signfies these to have only 1 valid alt allele, so lets use them ! 
+                        alt = alt.split(',')[0]               
+                        gvcf_hashmap[chrom+'_'+pos] = [rsid,ref,alt,gt,dp,alt_type]
+                
+    return gvcf_hashmap
 
 def parse_map_file(map_file):
-    """ Function to parse a map file and return the positions 
+    """ Function to parse a map file and return the ordered positions 
     Args : map_file ; string ; the map file
     Returns : list ; the coordinates in the map file
     """
 
-    coordinates = defaultdict(lambda: -1)
+    #coordinates_info = defaultdict(lambda: -1)
+    coordinates = OrderedDict()
     with open(map_file,'r') as MAP:
         for line in MAP:
-            pos = line.strip('\n').split('\t')[-1]
-            chrom = line.strip('\n').split('\t')[0]
-            coordinates[chrom+'_'+pos] = 0 
+            contents = line.strip('\n').split('\t')
+            pos = contents[-1]
+            chrom = contents[0]
+            rsid = contents[1]
+            #coordinates[chrom+'_'+pos] = 0
+            coordinates[chrom+'_'+pos] = rsid
     
     return coordinates
             
@@ -162,7 +263,6 @@ def main(args):
         sex = '1'
     else:
         sex = '2'
-
     ## Create output directory if it doesnt exist
     out_dir = os.path.dirname(output_stem)
     if out_dir != '':
@@ -172,8 +272,12 @@ def main(args):
     ## The input markers
     coordinates = parse_map_file(markers)
     
-    genotypes_ped,locations_map = iterate_gvcf(gvcf,coordinates)
-   
+    #genotypes_ped,locations_map = iterate_gvcf(gvcf,coordinates)
+    gvcf_hashmap = iterate_gvcf(gvcf)
+    print "Loaded gvcf hashmap"
+    genotypes_ped,locations_map = return_pedmap(gvcf_hashmap,coordinates)
+    
+    
     temp = [sample_id,family_id,'0','0',sex,pheno]
     temp.extend(genotypes_ped)
     output_ped_line = ' '.join(temp)
@@ -187,7 +291,6 @@ def main(args):
     ## Print output files
     print_output(output_ped_line,output_stem+'.ped')
     print_output(output_map_line,output_stem+'.map')
-    
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Create a ped file from a gvcf file',description='Takes as input : \n1. A valid gvcf file\n2. Marker loci\nOutput : A ped file containing genotype information at the appropriate marker loci')
