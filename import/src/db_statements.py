82e71e48e48a5a580026daa51da14f7ea1cff0de
brcopeland <bc2675@cumc.columbia.edu>
2016-09-23 21:51:59 -0400

    Implemented loading DP & GQ binning data.

--- import/src/db_statements.py
+++ import/src/db_statements.py
@@ -91,7 +91,7 @@ WHERE sample_id = {sample_id}
 GET_PIPELINE_STEP_ID = """
 SELECT id
 FROM pipeline_step
-WHERE description = "Imported Chromosome {chromosome}"
+WHERE description = "Imported Chromosome {chromosome} {data_type}"
 """
 GET_PIPELINE_FINISHED_ID = """
 SELECT id
@@ -126,4 +126,9 @@ INNER JOIN (
     GROUP BY sample_id) AS s2
     ON s1.sample_id = s2.sample_id AND s1.pipeline_step_id = {pipeline_step_id}
 SET s1.submit_time = s2.st
+"""
+INSERT_BIN_STATEMENT = """
+LOAD DATA INFILE '{data_file}' INTO TABLE {bin_type}_bins_chr{chromosome}
+    (@block_id, @bin_string)
+    SET sample_id={sample_id}, block_id=@block_id, {bin_type}_string=@bin_string
 """
