d836cfb44408209f2f1fd6bd697d44a06d4331e4
brcopeland <bc2675@cumc.columbia.edu>
2016-11-10 22:55:50 -0500

    Miscellaneous updates/fixes to running commands.

--- import/src/ProcessSamples.py
+++ import/src/ProcessSamples.py
@@ -15,7 +15,7 @@ def preexec_function():
     # ignore SIGINT by setting to SIG_IGN
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-class ProcessSamples():
+class ProcessSamples(object):
     def __init__(self, max_samples_concurrently, qdel_jobs=True, **kwargs):
         self.max_samples_concurrently = max_samples_concurrently
         self.qdel_jobs = qdel_jobs
@@ -39,20 +39,20 @@ class ProcessSamples():
     def _get_samples(self):
         # overwrite to get samples from DB, check files, etc.
         # should return a list of tuples of:
-        # (sample_name, sample_type, capture_kit, prep_id (pseudo_prep_id), priority)
+        # (sample_name, priority, (...))
+        # an example would be (sample_type, priority, (capture_kit, prep_id (pseudo_prep_id)))
         pass
 
-    def _get_command(self, sample_name, sample_type, capture_kit, prep_id):
+    def _get_command(self, sample_name, *args):
         # overwrite to format the actual command to run, and specify the timeout
         pass
 
-    def get_sge_job_ids(self, sample_name, sample_type, capture_kit, prep_id):
+    def get_sge_job_ids(self, sample_name, *args):
         """Pass in a pattern= parameter upon initialization if this method is to
         be used"""
         p = re.compile(r"^(\d+) ")
-        job_name = self.pattern.format(
-            sample_name=sample_name, sample_type=sample_type,
-            capture_kit=capture_kit, prep_id=prep_id, **self.kwargs)
+        job_name = re.compile(self.pattern.format(
+            sample_name=sample_name, *args, **self.kwargs))
         proc = subprocess.Popen(["qstat", "-r", "-ne"], stdout=subprocess.PIPE,
                                 preexec_fn=preexec_function)
         qstat_output = proc.communicate()[0]
@@ -65,25 +65,23 @@ class ProcessSamples():
                     job_ids.append(job_id)
         return job_ids
 
-    def run_command(self, sample_name, sample_type, capture_kit, prep_id):
+    def run_command(self, sample_name, *args):
         # overwrite to run one or more commands
-        cmd, timeout = self._get_command(
-            sample_name=sample_name, sample_type=sample_type,
-            capture_kit=capture_kit, prep_id=prep_id)
+        cmd, timeout = self._get_command(sample_name, *args)
+        print(cmd)
         command = Command.Command(cmd, preexec_fn=preexec_function)
         try:
             exit_code = command.run(timeout=timeout)
         except Command.TimeoutException:
             if self.qdel_jobs:
-                for sge_job in self.get_sge_job_ids(
-                    sample_name, sample_type, capture_kit, prep_id):
+                for sge_job in self.get_sge_job_ids(sample_name, *args):
                     p = subprocess.Popen(
                         ["qdel", sge_job], preexec_fn=preexec_function)
                     p.communicate()
-            sys.stderr("timed out importing {sample_name}\n".format(
+            sys.stderr("timed out processing {sample_name}\n".format(
                 sample_name=sample_name))
         if exit_code:
-            sys.stderr("failed importing {sample_name}\n".format(
+            sys.stderr("failed processing {sample_name}\n".format(
                 sample_name=sample_name))
 
     def process_samples(self):
@@ -110,19 +108,22 @@ class ProcessSamples():
                     break
             if done:
                 break
-            for sample_name, sample_type, capture_kit, prep_id, priority in (
-                self._get_samples()):
-                sample_metadata = (
-                    sample_name, sample_type, capture_kit, prep_id)
-                if sample_metadata not in self.samples_already_queued:
-                    self.samples_queue.put((priority, sample_metadata))
-                    self.samples_already_queued.add(sample_metadata)
+            for sample_name, priority, sample_metadata in self._get_samples():
+                if ((sample_name, sample_metadata)
+                    not in self.samples_already_queued):
+                    self.samples_queue.put(
+                        (priority, (sample_name, sample_metadata)))
+                    self.samples_already_queued.add(
+                        (sample_name, sample_metadata))
             if self.samples_queue.empty():
                 # sleep for a while since we don't have any samples to run
+                print("waiting for 300 seconds for new samples...")
                 sleep(300)
             else:
                 priority, sample_metadata = self.samples_queue.get()
+                sample_metadata = tuple(
+                    [sample_metadata[0]] + list(sample_metadata[1]))
                 thread = Thread(
-                    target=self.command, args=sample_metadata)
+                    target=self.run_command, args=sample_metadata)
                 self.all_threads.append(thread)
                 thread.start()
