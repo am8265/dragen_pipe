0c22885c0661d3745b2e6c9e8b1a3996271744cd
brcopeland <bc2675@cumc.columbia.edu>
2016-12-13 17:52:07 -0500

    Added new phasing associated columns to the variant call fields list.

--- import/src/dragen_globals.py
+++ import/src/dragen_globals.py
@@ -46,7 +46,8 @@ VARIANT_CALL_FORMAT = ("{" + "}\t{".join(
     ["sample_id", "variant_id", "block_id", "GT", "DP", "AD_REF",
      "AD_ALT", "GQ", "VQSLOD",
      "FS", "MQ", "QD", "QUAL", "ReadPosRankSum",
-     "MQRankSum", "FILTER", "highest_impact"]) + "}")
+     "MQRankSum", "FILTER", "highest_impact", "PID_variant_id", "PGT",
+     "HP_variant_id", "HP_GT", "PQ"]) + "}")
 NOVEL_VARIANT_OUTPUT_FORMAT = (
     "{" + "}\t{".join(
         ["variant_id", "POS", "REF", "ALT", "rs_number", "transcript_stable_id",
