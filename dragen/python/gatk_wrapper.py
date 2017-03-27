d96318444ee7ee1bcfbdd809dc3d3b5126980e15
rpadmanabhan <raghus606@gmail.com>
2017-03-27 12:25:18 -0400

    Updated wrapper script , stable working version, need to add email and logging to file

--- dragen/python/gatk_wrapper.py
+++ dragen/python/gatk_wrapper.py
@@ -1,22 +1,36 @@
 #!/nfs/goldstein/software/python2.7.7/bin/python2.7
 
 import argparse
+import glob
 import MySQLdb
 import os
 import shlex
 import string
 import sys
 import subprocess
-import time
+import signal
+import multiprocessing as mp 
+from time import sleep,time
+from threading import Thread
 import random
 from dragen_globals import get_connection
+from db_statements import * 
 from datetime import datetime
 
+
 ##############################################################################
-####     This script will submit a luigi job to run the gatk pipeline     ####
-####     qc metrics and update to database steps for a single sample      ####
-####              Add this to your crontab for automation                 ####
-##############################################################################                                                                                                                        
+###                   Automates submission of the gatk                     ###
+###                   luigi pipeline to the cluster                        ###                    
+##############################################################################
+
+## Define globals for storing tasks
+## Tasks are keyed by their name {sample_name}.{pseudo_prepid}.{sample_type}
+RUNNING = {}
+EXECUTED = {}
+ERRORS = {}
+email = None
+num_processes = 0
+
 def execute_query(database,query,fetch):
     """ Execute db fetch statements
 
@@ -26,72 +40,43 @@ def execute_query(database,query,fetch):
     
     return : Nothing
     """
-
-    tries = 3
-    for i in range(tries):
-        try:
-            db = get_connection(database)
-            cur = db.cursor()
-            cur.execute(query)
-            if fetch == True: ## For select statements 
-                result = cur.fetchone()
-                return result
-            else: ## For insert and delete statements
-                db.commit()
-        except MySQLdb.Error, e:
-            if i == tries - 1:
-                raise Exception("Error %d IN CONNECTION: %s" %(e.args[0],e.args[1]))
-            else:
-                time.sleep(random.randint(20,120)) ## Wait a random duration between 20 secs to a couple of minutes before trying again
-                continue 
-        finally:
-            db.close()
+    try:
+        db = get_connection(database)
+        cur = db.cursor()
+        cur.execute(query)
+        if fetch == True: ## For select statements 
+            result = cur.fetchall()
+            return result
+        else: ## For insert and delete statements
+            db.commit()
+    except MySQLdb.Error, e:
+        clean_tasks(force_exit=True)
+        raise Exception("Error %d IN CONNECTION: %s" %(e.args[0],e.args[1]))
+    finally:
+        db.close()
             
-def get_sample(args):
-    """ Get the next highest priority sample from the gatk_queue table
+def get_samples():
+    """ Get samples from the gatk_queue table
     """
-
-    query = ("SELECT * "
-             "FROM gatk_queue "
-             "WHERE PRIORITY < 99 "
-             "ORDER BY PRIORITY ASC")
-    
-    result = execute_query(args.database,query,fetch=True)
+    query = GET_SAMPLES_TO_RUN
+    result = execute_query("seqdb",query,fetch=True)
     return result
 
-def add_to_tmp(args,sample_info):
-    """ Add to the temp queue
-    """
-    
-    dragen_id,pseudo_prepid,sample_name,sample_type,capture_kit,priority,failed = sample_info[0:]
-    query = (""" INSERT INTO tmp_gatk ('dragen_id','pseudo_prepid','sample_name','sample_type','capture_kit','priority','failed') VALUES({0},{1}.{2},{3},{4},{5},{6})""".format(dragen_id,pseudo_prepid,sample_name,sample_type,capture_kit,priority,failed))
-    execute_query(args.database,query,fetch=False)
-
-def remove_sample(args,sample_info):
-    """ Remove the popped sample from the gatk_queue table
-    """
-    
-    dragen_id,pseudo_prepid,sample_name,sample_type,capture_kit,priority,failed = sample_info[0:]
-    query = (""" DELETE FROM gatk_queue WHERE sample_name = {0} AND sample_type = {1} AND pseudo_prepid = {2} limit 1""".format(sample_name,sample_type,pseudo_prepid))
-    execute_query(args.database,query,fetch=False)
-
-def create_logdir(baselogdir,sample_info):
+def create_logdir(baselogdir,sample_name,pseudo_prepid):
     """ Create luigi stdout and stderr logging directory for a sample
     
-    logdir : string ; Base logging directory
-    sample_info : list ; Sample information from gatk queue 
+    baselogdir : str ; Base logging directory
+    sample_name : str ; The sample name
+    pseudo_prepid : int ; The pseudo_prepid
 
     return : string ; Final path to the sample log directory 
     """
-
-    dragen_id,pseudo_prepid,sample_name,sample_type,capture_kit,priority,failed = sample_info[0:]
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d")
     sample_log_dir = os.path.join(timestamp+'_'+'logs',sample_name+'_'+str(pseudo_prepid))
     final_log_dir = os.path.join(baselogdir,sample_log_dir)
     if not os.path.exists(final_log_dir):
         os.makedirs(final_log_dir)
-        print final_log_dir
     return final_log_dir
 
 def get_capturekit_info(sample_type,capture_kit):
@@ -100,77 +85,192 @@ def get_capturekit_info(sample_type,capture_kit):
     capture_kit : string ; the capture kit type , e.g. Roche SeqCap EZ V3, 50MB, etc.
 
     returns : string ; the capture bed file location 
-    """
-    
+    """    
     if sample_type.upper() == 'GENOME':
          query = ("SELECT region_file_lsrc "
             "FROM captureKit WHERE prepT_name='Roche SeqCap EZ V3' "
             "AND chr = 'all' ")
-         result = execute_query(args.database,query,fetch=True)
-         return result[0]
+         result = execute_query("seqdb",query,fetch=True)
+         return result[0][0]
     else:
         query = ("SELECT region_file_lsrc "
                  "FROM captureKit WHERE prepT_name='{0}' "
                  "AND chr = 'all'").format(capture_kit)
-        result = execute_query(args.database,query,fetch=True)
-        return result[0]
+        result = execute_query("seqdb",query,fetch=True)
+        return result[0][0]
 
-def is_complete():
-    return False
-
-def run_pipeline(args,sample_info,final_log_dir):
-    """ Run a luigi job     
+def clean_tasks(force_exit = False,test=False):
+    """ Clean up luigi pipeline commands
     """
+    for name, task_details in RUNNING.items():
+        if not task_details[0].is_alive():
+            if task_details[0].exitcode:## Non zero return code, the task failed
+                ERRORS[name] = task_details[0].exitcode
+            else: ## Success
+                EXECUTED[name] = 'success!'
+                RUNNING.pop(name)
+        else: ## Still running , check if the running_time has exceeded
+            cur_time = datetime.now()
+            start_time = task_details[1]
+            diff = cur_time - start_time
+            time_elapsed = diff.total_seconds()
+            sample_type = name.split('.')[-1]
+            if force_exit == True:
+                print "Exiting wrapper; Killing sample %s ....\n"%name
+                kill_process(task_details[0])
+                kill_qstat_jobs(name, test = test)
+            else:
+                if sample_type.upper() == 'GENOME':
+                    if time_elapsed > 518400: ## Exceeded 6 days
+                        print "Genome sample %s exceeded 6 days Killing process ...\n"%name
+                        kill_process(task_details[0])
+                        kill_qstat_jobs(name,test=test)
+                        RUNNING.pop(name)
+                else:
+                    if time_elapsed > 54000: ## Exceeded 15 hrs
+                        print "Exome sample %s exceeded 15 hours Killing process ...\n"%name
+                        kill_process(task_details[0])
+                        kill_qstat_jobs(name,test=test)
+                        RUNNING.pop(name)
+   
+def kill_process(process):
+    """
+    process : a multiprocessing process object
+    """
+    pid = process.pid
+    os.kill(pid,signal.SIGKILL)
+
+def kill_qstat_jobs(task_name,test=False):
+    """
+    task_name : str; e.g. sample_name.pseudo_prepid.sample_type
+    test : bool ; whether to run in test mode 
+    """
+    if test:
+        return True
     
-    dragen_id,pseudo_prepid,sample_name,sample_type,capture_kit,priority,failed = sample_info[0:]
-    pseudo_prepid = str(pseudo_prepid)
-    capturekit_bed = get_capturekit_info(sample_type,capture_kit)
+    ## Since qstat jobs are defined as sample_name.pseudo_prepid in gatk_pipeline
+    job_name = '.'.join(task_name.split('.')[0:2])
+    shell_cmd = ("""qstat -r -ne | grep -B 1 %s|"""
+                 """cut -f1 -d' '|xargs qdel -j {}"""%(job_name))
+    proc = subprocess.Popen(shell_cmd,shell=True)
+    proc.wait()
+
+def test_luigi(sample_info,baselogdir):
+    """ This is a test target function for testing purposes
+    """
+    pseudo_prepid,sample_name,sample_type,capture_bed,scratch = sample_info
+    final_log_dir = create_logdir(baselogdir,sample_name,pseudo_prepid)
     out_file = os.path.join(final_log_dir,sample_name+'_'+pseudo_prepid+'.stdout')
     error_file = os.path.join(final_log_dir,sample_name+'_'+pseudo_prepid+'.stderr')
-    cmd = (""" luigi --module gatk_pipe ArchiveSample --sample-name {0} --pseudo-prepid {1} --capture-kit-bed  {2} --sample-type {3} --dont-remove-tmp-dir --no-tarball --poll-time 120 --workers 2 --worker-wait-interval 180 --scheduler-remove-delay 86400 >> {4} 2>>{5}""".format(sample_name,pseudo_prepid,capturekit_bed,sample_type,out_file,error_file))
-    print cmd
-    
-    """
-    proc = subprocess.Popen(cmd,shell=True)
-    proc.wait()
-    if proc.returncode: ## Non zero return code
-        ## There are a few options here ;
-        ## 1. Re-run the pipeline, luigi already does this and it might be worth increasing the retry attempts
-        ## 2. Log the failure in database and rerun at some other point in time, this info can
-        ##    already be obtain from the dragen_pipeline_step table
-        ## 3. Wait for a random time and rerun the command again, this might be a good option for now
-        
-        ## Note : Recursion is also a good option, as long as this wrapper is bug free, it will give us
-        ## a window of opppurtunity to fix the pipeline code before recursing for another try upon
-        ## failure. This would be the final solution for complete automation  
-        
-        ## Going with option 3 for now, go to sleep :)
-        time.sleep(random.randint(3000,10000)) ## Wait a random time between 50 and 166 minutes
 
-        ## We could be on to the next day, so check for the existence and create a logdir if it
-        ## does not exist yet for this sample
-        final_log_dir = create_logdir(args.baselogdir,sample_info)
-        out_file = os.path.join(final_log_dir,sample_name+'_'+pseudo_prepid+'.stdout')
-        error_file = os.path.join(final_log_dir,sample_name+'_'+pseudo_prepid+'.stderr')
-    """
-        #cmd = (""" luigi --module gatk_pipe ArchiveSample --sample-name {0} --pseudo-prepid {1} --capture-kit-bed  {2} --sample-type {3} --dont-remove-tmp-dir --no-tarball --poll-time 120 --workers 2 --worker-wait-interval 180 --scheduler-remove-delay 86400 >> {4} 2>>{5}""".format(sample_name,pseudo_prepid,capturekit_bed,sample_type,out_file,error_file))
-        
+    try:
+        flag = 0
+        key = sample_name+'.'+pseudo_prepid+'.'+sample_type
+        cmd = (
+            """ echo luigi --module gatk_pipe ArchiveSample --sample-name {0} --pseudo-prepid {1} --capture-kit-bed  {2} --sample-type {3} --scratch {4} --poll-time 120 --workers 2 --worker-wait-interval 180 --scheduler-remove-delay 86400 >> {5} 2>>{6} & sleep 100""".format(sample_name,pseudo_prepid,capture_bed,sample_type,scratch,out_file,error_file))
+        proc = subprocess.Popen(cmd, shell = True)
+        proc.wait()
+        exit(proc.returncode)
+    except KeyboardInterrupt:
+        flag = 1
+    finally:
+        if flag == 1:
+            print "Keyboard interrupt, cleaning up process ... {0}.{1} \n".format(sample_name,pseudo_prepid)
     
+def run_luigi(sample_info,baselogdir):
+    """ Execute the luigi command with the appropriate parameters as a system call     
+
+    sample_info : tuple ; (pseudo_prepid,sample_name,...)
+    baselogdir : str; base directory to store the logs ; e.g. /nfs/seqscratch09/luigi_logs_test/
+    """   
+    pseudo_prepid,sample_name,sample_type,capture_bed,scratch = sample_info
+    final_log_dir = create_logdir(baselogdir,sample_name,pseudo_prepid)
+    out_file = os.path.join(final_log_dir,sample_name+'_'+pseudo_prepid+'.stdout')
+    error_file = os.path.join(final_log_dir,sample_name+'_'+pseudo_prepid+'.stderr')
+    
+    try:
+        flag = 0
+        key = sample_name+'.'+pseudo_prepid+'.'+sample_type
+        #cmd = (""" luigi --module gatk_pipe ArchiveSample --sample-name {0} --pseudo-prepid {1} --capture-kit-bed  {2} --sample-type {3} --scratch {4} --poll-time 120 --workers 2 --worker-wait-interval 180 --scheduler-remove-delay 86400 >> {5} 2>>{6}""".format(sample_name,pseudo_prepid,capture_bed,sample_type,scratch,out_file,error_file))
+        cmd = (""" luigi --module gatk_pipe ArchiveSample --sample-name {0} --pseudo-prepid {1} --capture-kit-bed  {2} --sample-type {3} --scratch {4} --poll-time 120 --workers 2 --worker-wait-interval 180 --scheduler-remove-delay 86400""".format(sample_name,pseudo_prepid,capture_bed,sample_type,scratch))
+        with open(out_file,'w') as OUT, open(error_file,'w') as ERR:
+            proc = subprocess.Popen(shlex.split(cmd),stdout=OUT,stderr=ERR)
+            proc.wait()
+            exit(proc.returncode)            
+    except KeyboardInterrupt:
+        flag = 1
+    finally:
+        if flag == 1:
+            print "Keyboard interrupt, cleaning up process ... {0}.{1} \n".format(sample_name,pseudo_prepid)
+        
+def start_pipeline(sample_info,baselogdir,test=False):
+    """ Start a luigi gatk pipeline     
+    sample_info : tuple ; parameters for running a luigi job
+    baselogdir : str ; the base directory to store the logs
+    test : bool ; whether to run in test mode
+    """
+    pseudo_prepid,sample_name,sample_type,capture_bed,scratch = sample_info
+    name = sample_name+'.'+pseudo_prepid+'.'+sample_type
+    if name not in RUNNING:
+        if test:
+            p = mp.Process(target=test_luigi,args=[sample_info,baselogdir],name=name)
+        else:
+            p = mp.Process(target=run_luigi,args=[sample_info,baselogdir],name=name)
+        RUNNING[name] = (p,datetime.now())
+        p.start()
+        
+   
 def main(args):
     """ The main function
-    """
-    
-    while True:
-        sample_info = get_sample(args)
-        #add_to_tmp(args,sample_info)
-        final_log_dir = create_logdir(args.baselogdir,sample_info)
-        run_pipeline(args,sample_info,final_log_dir)
-        #if is_complete(sample_info):
-            #remove_sample(args,sample_info)
-                           
+    """    
+    ## Unpack parameters 
+    ## Dont want to pass email around to clean_tasks,keeping it global should be pretty safe
+    global email
+    email = args.email
+    baselogdir = args.baselogdir
+    max_processes = args.max_processes
+    wait_time = args.wait_time
+    scratch = "/nfs/%s/ALIGNMENT/BUILD37/DRAGEN"%args.scratch
+    test = args.test
+
+    try:
+        while True:
+            for pseudo_prepid,sample_name,sample_type,capture_kit,priority in get_samples():
+                task_key = sample_name+'.'+str(pseudo_prepid)+'.'+sample_type
+                if task_key not in RUNNING and task_key not in ERRORS:
+                    capture_bed = get_capturekit_info(sample_type,capture_kit)
+                    sample_tup = (str(pseudo_prepid),sample_name,sample_type,capture_bed,scratch)
+                    wild_card_bam = glob.glob(scratch+'/%s/%s.%s/%s.%s*.bam'%(sample_type.upper(),sample_name,pseudo_prepid,sample_name,pseudo_prepid))
+                    if len(RUNNING.keys()) <= max_processes:
+                        if len(wild_card_bam)>0:
+                            start_pipeline(sample_tup,baselogdir,test=test)
+                        else:
+                            print "Will not run {0}.{1} bam not found !".format(sample_name,pseudo_prepid)
+                    else:
+                        while len(RUNNING.keys()) >= max_processes: ## Hold till processes are freed up
+                            print "Tasks Executed : ",EXECUTED
+                            print "Tasks Running : ",RUNNING
+                            print "Failed Tasks :",ERRORS
+                            print "Task Queued : ",task_key                            
+                            sleep(wait_time)
+                            clean_tasks(force_exit = False,test=test)            
+    except KeyboardInterrupt:
+        print "Keyboard interrupt in main"
+        for key in RUNNING:
+            kill_process(RUNNING[key][0])
+            kill_qstat_jobs(key)
+    finally:
+        print "Exiting Main....\n"
+        sys.exit()                          
+        
+                               
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('Luigi wrapper script',description="This script is a wrapper for running the new dragen variant calling and qc pipeline. It is created for a very specific use case i.e. getting sample information from the table gatk_queue in sequenceDB , adding it to the table tmp_queue , deleting that entry from gatk_queue and submitting a luigi job for the sample. To automate this process add this script to your cron tab.")
-    parser.add_argument("--database",default="seqdb",help="The db prefix in the cnf file")
-    parser.add_argument("--baselogdir",default="/nfs/seqscratch09/pipeline_logs/",help="Directory to log the luigi stdout and stderr, will create a timestamp specific and a sample specific directory in here, timestamp is at the level of a day")
+    parser = argparse.ArgumentParser('Luigi wrapper script',description="This script is a wrapper for running the new dragen variant calling and qc pipeline.")
+    parser.add_argument("--baselogdir",default="/nfs/seqscratch09/pipeline_logs/",help="Directory to log the luigi stdout and stderr, will create a timestamp and sample specific directory in here, timestamp is at the level of a day",required = False)
+    parser.add_argument("--max-processes",dest='max_processes',default=300,help="The maximum number of processes to run",required=False,type=int)
+    parser.add_argument("--wait-time",dest='wait_time',default=3600,help="The number of seconds to wait before querying the database for samples again",required=False,type=int)
+    parser.add_argument("--scratch",default="seqscratch_ssd",help="The scratch directory to operate on",required=False)
+    parser.add_argument("--email",default=None,help="Email to send notifications to",required=False)
+    parser.add_argument("--test",action='store_true',help="Will simply echo the luigi commands instead of running them")
     args = parser.parse_args()
     main(args)
